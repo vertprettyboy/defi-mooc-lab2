8650b752f49c6cbc1aba68c7a8fbfe1921e50bd3
vertprettyboy <pattarapol.set@ku.th>
2026-03-24 23:30:57 +0700

    fix: update IAaveLendingPool with correct Aave V2 interface

--- contracts/interfaces/IAaveLendingPool.sol
+++ contracts/interfaces/IAaveLendingPool.sol
@@ -8,5 +8,5 @@ interface IAaveLendingPool {
         address user,
         uint256 debtToCover,
         bool receiveAToken
-    ) external;
+    ) external; // Aave V2 liquidationCall ไม่มีการ return ค่า
 }
